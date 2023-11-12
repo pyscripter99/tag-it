4d112e60d11767ec0915d83e8d30114ec65472b2
pyscripter99 <ryder@retzlaff.family>
2023-11-13 09:42:52 +1100

    Add logic to bump command

--- cmd/cli/cmd/bump.go
+++ cmd/cli/cmd/bump.go
@@ -4,19 +4,48 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strings"
 	"tag-it/internal/tagger"
 
 	"github.com/Masterminds/semver"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/spf13/cobra"
 )
 
 var bumpCmd = &cobra.Command{
 	Use:   "bump <patch|minor|major> [version] [flags]",
 	Short: "Bump the version by specified amount",
-	Long:  "Bump the version by specified amount",
+	Long:  "Bump the version by specified amount.\nIf authentication is required set: GIT_USERNAME and GIT_PASSWORD environment variables",
 	Run: func(cmd *cobra.Command, args []string) {
 		useGit, err := cmd.Flags().GetBool("git")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		usePrefix, err := cmd.Flags().GetBool("prefix")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		usePush, err := cmd.Flags().GetBool("push")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		useCommit := usePush
+		if !usePush {
+			useCommit, err = cmd.Flags().GetBool("commit")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		tagMessage, err := cmd.Flags().GetString("tag-message")
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -43,20 +72,23 @@ var bumpCmd = &cobra.Command{
 		}
 
 		var ver *semver.Version
+		var repo *git.Repository
 
-		if useGit {
+		if useGit || useCommit || usePush {
 			repositoryPath, err := cmd.Flags().GetString("repository")
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
 
-			repo, err := git.PlainOpen(repositoryPath)
+			repo, err = git.PlainOpen(repositoryPath)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
+		}
 
+		if useGit {
 			ver, err = tagger.LoadGitLatestVersion(repo)
 			if err != nil {
 				fmt.Println(err)
@@ -86,6 +118,58 @@ var bumpCmd = &cobra.Command{
 		absVer := *ver
 		absVer = tagger.BumpVersion(absVer, bump)
 		ver = &absVer
+
+		var verString string
+		if usePrefix {
+			verString = strings.Join([]string{"v", ver.String()}, "")
+		}
+		if !usePrefix {
+			verString = ver.String()
+		}
+
+		if useCommit {
+			ref, err := repo.Head()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			_, err = repo.CreateTag(verString, ref.Hash(), &git.CreateTagOptions{
+				Message: tagMessage,
+			})
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		if usePush {
+			var username, password string
+
+			for _, env := range os.Environ() {
+				envSplit := strings.Split(env, "=")
+				if envSplit[0] == "GIT_USERNAME" {
+					username = envSplit[1]
+				}
+				if envSplit[0] == "GIT_PASSWORD" {
+					password = envSplit[1]
+				}
+			}
+
+			var auth *http.BasicAuth
+
+			if username != "" && password != "" {
+				auth = &http.BasicAuth{
+					Username: username,
+					Password: password,
+				}
+			}
+
+			if err := tagger.PushTags(repo, "origin", auth); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
 	},
 }
 
@@ -93,8 +177,9 @@ func init() {
 	rootCmd.AddCommand(bumpCmd)
 
 	bumpCmd.Flags().BoolP("git", "g", false, "Specifies if tag-it should get the current version from the current git repository")
-	bumpCmd.Flags().StringP("push", "p", "", "Create git commit and push to specified remote")
+	bumpCmd.Flags().BoolP("push", "p", false, "Create git commit and push to specified remote")
 	bumpCmd.Flags().BoolP("commit", "c", false, "Specifies if the new tag should be committed to the current git repository.")
-	bumpCmd.Flags().String("commit-message", "Bump version to '$version$'", "String to use in commit message.")
+	bumpCmd.Flags().String("tag-message", "Bump version to '$version$'", "String to use in commit message.")
 	bumpCmd.Flags().StringP("repository", "r", ".", "Folder containing .git/")
+	bumpCmd.Flags().Bool("prefix", false, "Adds 'v' to beginning of version")
 }
--- internal/tagger/git.go
+++ internal/tagger/git.go
@@ -2,11 +2,15 @@ package tagger
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"sort"
 
 	"github.com/Masterminds/semver"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport"
 )
 
 func LoadGitLatestVersion(repo *git.Repository) (*semver.Version, error) {
@@ -37,4 +41,25 @@ func LoadGitLatestVersion(repo *git.Repository) (*semver.Version, error) {
 	}
 
 	return tags[len(tags)-1], err
+}
+
+func PushTags(r *git.Repository, remoteName string, auth transport.AuthMethod) error {
+	po := &git.PushOptions{
+		RemoteName: remoteName,
+		Progress:   os.Stdout,
+		RefSpecs:   []config.RefSpec{config.RefSpec("refs/tags/*:refs/tags/*")},
+		Auth:       auth,
+	}
+
+	err := r.Push(po)
+	if err != nil {
+		if err == git.NoErrAlreadyUpToDate {
+			fmt.Println("origin remote was up to date, no push done")
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
 }
